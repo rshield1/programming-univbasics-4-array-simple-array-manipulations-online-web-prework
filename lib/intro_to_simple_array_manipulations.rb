3d8ddd3d8416b75bd4ba2ca9c305d444e31fa167
Robert Shields <robert.shieldsiv@gmail.com>
2019-12-05 15:40:08 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -5,3 +5,19 @@ end
 def using_unshift(array, string)
   array.unshift(string)
 end
+
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
