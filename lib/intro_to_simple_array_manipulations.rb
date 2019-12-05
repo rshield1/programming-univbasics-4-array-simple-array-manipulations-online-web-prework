611fbcbe94e7350b18cffffa92d2650d426a4159
Robert Shields <robert.shieldsiv@gmail.com>
2019-12-05 15:44:45 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -6,18 +6,22 @@ def using_unshift(array, string)
   array.unshift(string)
 end
 
-def using_push(array, string)
-  array.push(string)
+def using_pop(array)
+  array.pop()
 end
 
-def using_unshift(array, string)
-  array.unshift(string)
+def pop_with_args(array)
+  array.pop(2)
 end
 
-def using_push(array, string)
-  array.push(string)
+def using_shift(array)
+  array.shift()
 end
 
-def using_unshift(array, string)
-  array.unshift(string)
+def shift_with_args(array)
+  array.shift(2)
+end
+
+def using_concat(array1, array2)
+  
 end
